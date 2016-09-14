18a076ac82638e1d9338136bd5750fa5b0687d68
Max Kellermann <max.kellermann@gmail.com>
2016-09-14 20:26:26 +0200

    Workplace: use C++11 initializer

--- src/Workplace.hxx
+++ src/Workplace.hxx
@@ -24,13 +24,14 @@ class Workplace {
 
     typedef std::list<Operator *> OperatorList;
     OperatorList operators;
+    unsigned num_operators = 0;
 
-    unsigned max_operators, num_operators;
+    unsigned max_operators;
 
 public:
     Workplace(const char *_node_name, unsigned _max_operators)
         :node_name(_node_name),
-         max_operators(_max_operators), num_operators(0) {
+         max_operators(_max_operators) {
         assert(max_operators > 0);
     }
 
