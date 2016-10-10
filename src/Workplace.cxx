c43cedd2e9b6d36dc8353a1e3e35c3f9c66d6c67
Max Kellermann <max.kellermann@gmail.com>
2016-10-10 19:16:54 +0200

    Workplace: use std::throw_with_nested()
    std::rethrow_if_nested() was the wrong thing to call here.

--- src/Workplace.cxx
+++ src/Workplace.cxx
@@ -127,7 +127,7 @@ Workplace::Start(EventLoop &event_loop, const Job &job,
             o->syslog.reset(SyslogClient::Create(node_name.c_str(), ident, 1,
                                                  job.syslog_server.c_str()));
         } catch (const std::runtime_error &e) {
-            std::rethrow_if_nested(FormatRuntimeError("syslog_open(%s) failed",
+            std::throw_with_nested(FormatRuntimeError("syslog_open(%s) failed",
                                                       job.syslog_server.c_str()));
         }
 
