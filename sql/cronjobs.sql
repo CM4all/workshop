ca90c195d5c46ea0f2f205bbf1cbbfc904b7dc24
Max Kellermann <max.kellermann@gmail.com>
2016-10-13 17:43:22 +0200

    sql/cronjobs.sql: first draft of the "cronjobs" table
    For the upcoming cron daemon based on Workshop.

---
+++ sql/cronjobs.sql
@@ -0,0 +1,72 @@
+--
+--  Create the "cronjobs" table
+--
+--  author: Max Kellermann <mk@cm4all.com>
+--
+
+CREATE TABLE cronjobs (
+    --------------------------------
+    -- Internal PostgreSQL columns
+    --------------------------------
+
+    id serial PRIMARY KEY,
+
+    --------------------------------
+    -- Execution parameters
+    --------------------------------
+
+    -- the owner's account id
+    account_id varchar(32) NOT NULL,
+
+    -- the schedule in crontab(5) syntax
+    schedule varchar(256) NOT NULL,
+
+    -- the command to be executed by /bin/sh
+    command varchar(1024) NOT NULL,
+
+    -- a parameter for the translation server
+    translate_param varchar(1024) NULL,
+
+    -- is this cronjob enabled?
+    enabled boolean NOT NULL DEFAULT TRUE,
+
+    -- shall overlapped execution of slow instances be allowed?
+    overlapping boolean NOT NULL DEFAULT TRUE,
+
+    -- ????
+    notification varchar(512) NULL,
+
+    --------------------------------
+    -- Scheduler state/control (calculated)
+    --------------------------------
+
+    -- the last time this job was run; NULL means never
+    last_run timestamp NULL,
+
+    -- the next time this job will run; this was calculated from the schedule
+    next_run timestamp NULL,
+
+    -- which node is executing this job?
+    node_name varchar(256) NULL,
+    -- which time can we assume the node is dead?
+    node_timeout timestamp NULL,
+
+    --------------------------------
+    -- UI columns
+    --------------------------------
+
+    -- human readable long description of this job
+    description varchar(4096) NULL
+);
+
+-- find scheduled jobs
+CREATE INDEX cronjobs_scheduled ON cronjobs(next_run)
+    WHERE enabled AND node_name IS NULL AND next_run IS NOT NULL;
+
+-- for finding jobs to release
+CREATE INDEX cronjobs_release ON cronjobs(node_name, node_timeout)
+    WHERE node_name IS NOT NULL;
+
+-- notify all nodes when a new cronjob is added
+CREATE OR REPLACE RULE new_cronjob AS ON INSERT TO cronjobs
+    DO NOTIFY cronjobs_scheduled;
