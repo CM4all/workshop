f1ca98556b21bcc8504d15a53212f6f0a9d222a1
Max Kellermann <mk@cm4all.com>
2017-06-23 11:12:42 +0200

    workshop/LogBridge: un-inline the constructor

--- src/workshop/LogBridge.cxx
+++ src/workshop/LogBridge.cxx
@@ -5,6 +5,17 @@
 #include "LogBridge.hxx"
 #include "util/StringView.hxx"
 
+LogBridge::LogBridge(EventLoop &event_loop,
+                     UniqueFileDescriptor &&read_pipe_fd,
+                     const char *host_and_port,
+                     const char *me, const char *ident,
+                     int facility)
+    :reader(event_loop, std::move(read_pipe_fd),
+            BIND_THIS_METHOD(OnStderrLine)),
+     client(host_and_port, me, ident, facility)
+{
+}
+
 bool
 LogBridge::OnStderrLine(WritableBuffer<char> line)
 {
--- src/workshop/LogBridge.hxx
+++ src/workshop/LogBridge.hxx
@@ -19,10 +19,7 @@ public:
     LogBridge(EventLoop &event_loop, UniqueFileDescriptor &&read_pipe_fd,
               const char *host_and_port,
               const char *me, const char *ident,
-              int facility)
-        :reader(event_loop, std::move(read_pipe_fd),
-                BIND_THIS_METHOD(OnStderrLine)),
-         client(host_and_port, me, ident, facility) {}
+              int facility);
 
     void Flush() {
         reader.Flush();
