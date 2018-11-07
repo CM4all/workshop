e9d5b17a1dd5c337140aa92b0177ea04979e47e4
Max Kellermann <mk@cm4all.com>
2018-11-07 10:24:08 +0100

    Migrate: eliminate CheckCreateRule()

--- src/Migrate.cxx
+++ src/Migrate.cxx
@@ -43,20 +43,10 @@
 #include <stdlib.h>
 
 static void
-CheckCreateRule(Pg::Connection &c, const char *schema,
-		const char *table_name, const char *rule_name,
-		const char *create_statement)
-{
-	if (Pg::RuleExists(c, schema, table_name, rule_name))
-		return;
-
-	printf("Creating rule %s.%s\n", table_name, rule_name);
-	c.Execute(create_statement);
-}
-
-static void
 MigrateWorkshopDatabase(Pg::Connection &c, const char *schema)
 {
+	(void)schema;
+
 	/* since Workshop 2.0.13 */
 	c.Execute("ALTER TABLE jobs ADD COLUMN IF NOT EXISTS log text NULL");
 
@@ -71,11 +61,10 @@ MigrateWorkshopDatabase(Pg::Connection &c, const char *schema)
 	c.Execute("CREATE INDEX IF NOT EXISTS jobs_scheduled2 ON jobs(scheduled_time)"
 		  " WHERE enabled AND node_name IS NULL AND time_done IS NULL AND exit_status IS NULL AND scheduled_time IS NOT NULL;");
 
-	CheckCreateRule(c, schema, "jobs", "job_enabled",
-			"CREATE OR REPLACE RULE job_enabled AS ON UPDATE TO jobs"
-			" WHERE NOT OLD.enabled AND NEW.enabled"
-			" AND NEW.node_name IS NULL AND NEW.time_done IS NULL AND NEW.exit_status IS NULL"
-			" DO SELECT pg_notify('new_job', NULL)");
+	c.Execute("CREATE OR REPLACE RULE job_enabled AS ON UPDATE TO jobs"
+		  " WHERE NOT OLD.enabled AND NEW.enabled"
+		  " AND NEW.node_name IS NULL AND NEW.time_done IS NULL AND NEW.exit_status IS NULL"
+		  " DO SELECT pg_notify('new_job', NULL)");
 
 	/* since Workshop 2.0.37 */
 	c.Execute("ALTER TABLE jobs ADD COLUMN IF NOT EXISTS env varchar(4096)[] NULL");
