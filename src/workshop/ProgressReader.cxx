CM4all/workshop

commit 43afec463150590ce530bc7041479a3dfd5a8f8f

event/SocketEvent: add method Close()

src/workshop/ProgressReader.cxx
@@ -31,13 +31,13 @@
  */
 
 #include "ProgressReader.hxx"
+#include "io/UniqueFileDescriptor.hxx"
 
 ProgressReader::ProgressReader(EventLoop &event_loop,
 			       UniqueFileDescriptor _fd,
 			       Callback _callback) noexcept
-	:fd(std::move(_fd)),
-	 event(event_loop, BIND_THIS_METHOD(PipeReady),
-	       SocketDescriptor::FromFileDescriptor(fd)),
+	:event(event_loop, BIND_THIS_METHOD(PipeReady),
+	       SocketDescriptor::FromFileDescriptor(_fd.Release())),
 	 callback(_callback)
 {
 	event.ScheduleRead();
@@ -50,6 +50,7 @@ ProgressReader::PipeReady(unsigned) noexcept
 	ssize_t nbytes, i;
 	unsigned new_progress = 0, p;
 
+	FileDescriptor fd(event.GetSocket().ToFileDescriptor());
 	nbytes = fd.Read(buffer, sizeof(buffer));
 	if (nbytes <= 0) {
 		event.Cancel();

src/PipeCaptureBuffer.cxx
@@ -31,13 +31,13 @@
  */
 
 #include "PipeCaptureBuffer.hxx"
+#include "io/UniqueFileDescriptor.hxx"
 
 PipeCaptureBuffer::PipeCaptureBuffer(EventLoop &event_loop,
 				     UniqueFileDescriptor _fd,
 				     size_t capacity) noexcept
-	:fd(std::move(_fd)),
-	 event(event_loop, BIND_THIS_METHOD(OnSocket),
-	       SocketDescriptor::FromFileDescriptor(fd)),
+	:event(event_loop, BIND_THIS_METHOD(OnSocket),
+	       SocketDescriptor::FromFileDescriptor(_fd.Release())),
 	 buffer(capacity)
 {
 	event.ScheduleRead();
@@ -46,6 +46,8 @@ PipeCaptureBuffer::PipeCaptureBuffer(EventLoop &event_loop,
 void
 PipeCaptureBuffer::OnSocket(unsigned) noexcept
 {
+	FileDescriptor fd(event.GetSocket().ToFileDescriptor());
+
 	auto w = buffer.Write();
 	if (!w.empty()) {
 		ssize_t nbytes = fd.Read(w.data, w.size);

src/PipeCaptureBuffer.hxx
@@ -33,15 +33,15 @@
 #pragma once
 
 #include "CaptureBuffer.hxx"
-#include "io/UniqueFileDescriptor.hxx"
 #include "event/SocketEvent.hxx"
+
+class UniqueFileDescriptor;
 
 /**
  * Capture up to 8 kB of data from a pipe asynchronously.  This is
  * useful to capture the output of a child process.
  */
 class PipeCaptureBuffer {
-	UniqueFileDescriptor fd;
 	SocketEvent event;
 
 	CaptureBuffer buffer;
@@ -50,7 +50,9 @@ public:
 	explicit PipeCaptureBuffer(EventLoop &event_loop,
 				   UniqueFileDescriptor _fd,
 				   size_t capacity) noexcept;
-	virtual ~PipeCaptureBuffer() noexcept = default;
+	virtual ~PipeCaptureBuffer() noexcept {
+		Close();
+	}
 
 	bool IsFull() const noexcept {
 		return buffer.IsFull();
@@ -78,8 +80,7 @@ protected:
 
 private:
 	void Close() noexcept {
-		event.Cancel();
-		fd.Close();
+		event.Close();
 	}
 
 	void OnSocket(unsigned events) noexcept;

src/cron/SpawnOperator.cxx
@@ -36,6 +36,7 @@
 #include "spawn/Interface.hxx"
 #include "spawn/Prepared.hxx"
 #include "system/Error.hxx"
+#include "io/UniqueFileDescriptor.hxx"
 #include "util/Exception.hxx"
 #include "util/UTF8.hxx"
 

src/workshop/ProgressReader.hxx
@@ -33,12 +33,12 @@
 #pragma once
 
 #include "event/SocketEvent.hxx"
-#include "io/UniqueFileDescriptor.hxx"
 #include "util/BindMethod.hxx"
 #include "util/StaticArray.hxx"
 
+class UniqueFileDescriptor;
+
 class ProgressReader {
-	UniqueFileDescriptor fd;
 	SocketEvent event;
 	StaticArray<char, 64> stdout_buffer;
 	unsigned last_progress = 0;
@@ -50,6 +50,10 @@ public:
 	ProgressReader(EventLoop &event_loop,
 		       UniqueFileDescriptor _fd,
 		       Callback _callback) noexcept;
+
+	~ProgressReader() noexcept {
+		event.Close();
+	}
 
 private:
 	void PipeReady(unsigned) noexcept;

commit 8f05a8876dcb8d435c1ef76965653697e3c1b6d0

workshop/ProgressReader: fix double close bug
Fixes regression by commit 43afec463150590ce530bc7041479a3dfd5a8f8f
which caused hanging Workshop processes due to file descriptor
corruption after it was closed twice.

That commit caused the file descriptor to be copied (not moved) from
the SocketEvent to a local variable, which was then closed; however
the file descriptor was still registered in the SocketEvent, and will
be closed again in the destructor.  If somebody else just happens to
occupy that file descriptor number meanwhile, it will be closed,
and can be allocated again by somebody else, causing all kinds of
strange behavior.

The simple fix is to close the SocketEvent instead of the
FileDescriptor copy.  This way, the SocketEvent can properly update
its internal state, and makes the second SocketEvent::Close() call in
the destructor a no-op.

src/workshop/ProgressReader.cxx
@@ -53,8 +53,7 @@ ProgressReader::PipeReady(unsigned) noexcept
 	FileDescriptor fd(event.GetSocket().ToFileDescriptor());
 	nbytes = fd.Read(buffer, sizeof(buffer));
 	if (nbytes <= 0) {
-		event.Cancel();
-		fd.Close();
+		event.Close();
 		return;
 	}
 