a3d3c0912e73698a1cefaffb25361ce9789c1ed4
Max Kellermann <mk@cm4all.com>
2022-04-06 15:10:58 +0200

    increment version number to 6.0.1

--- doc/conf.py
+++ doc/conf.py
@@ -58,7 +58,7 @@ author = 'Max Kellermann'
 # built documents.
 #
 # The short X.Y version.
-version = '5.5'
+version = '6.0.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
