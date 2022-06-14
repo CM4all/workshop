6a68b4049644a5ad87c6665d6ad56457c0332b08
Max Kellermann <mk@cm4all.com>
2022-06-14 11:08:54 +0200

    translation/CronClient: move SendFull() to Send.cxx

--- src/translation/Send.cxx
+++ src/translation/Send.cxx
@@ -30,58 +30,23 @@
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "CronClient.hxx"
-#include "Marshal.hxx"
 #include "Send.hxx"
-#include "Receive.hxx"
-#include "translation/Protocol.hxx"
-#include "translation/Response.hxx"
-#include "AllocatorPtr.hxx"
 #include "net/SocketDescriptor.hxx"
+#include "net/SocketError.hxx"
 
-#include <stdexcept>
+#include <sys/socket.h>
 
-static void
-SendTranslateCron(SocketDescriptor s, const char *partition_name, const char *listener_tag,
-		  const char *user, const char *uri, const char *param)
+class SocketDescriptor;
+
+void
+SendFull(SocketDescriptor s, std::span<const std::byte> buffer)
 {
-	assert(user != nullptr);
+	ssize_t nbytes = send(s.Get(), buffer.data(), buffer.size(),
+			      MSG_NOSIGNAL);
+	if (nbytes < 0)
+		throw MakeSocketError("send() to translation server failed");
 
-	if (strlen(user) > 256)
-		throw std::runtime_error("User name too long");
-
-	if (param != nullptr && strlen(param) > 4096)
-		throw std::runtime_error("Translation parameter too long");
-
-	TranslationMarshaller m;
-
-	m.Write(TranslationCommand::BEGIN);
-
-	size_t partition_name_size = partition_name != nullptr
-		? strlen(partition_name)
-		: 0;
-	m.Write(TranslationCommand::CRON,
-		std::string_view{partition_name, partition_name_size});
-
-	if (listener_tag != nullptr)
-		m.Write(TranslationCommand::LISTENER_TAG, listener_tag);
-
-	m.Write(TranslationCommand::USER, user);
-	if (uri != nullptr)
-		m.Write(TranslationCommand::URI, uri);
-	if (param != nullptr)
-		m.Write(TranslationCommand::PARAM, param);
-	m.Write(TranslationCommand::END);
-
-	SendFull(s, m.Commit());
+	if (size_t(nbytes) != buffer.size())
+		throw std::runtime_error("Short send() to translation server");
 }
 
-TranslateResponse
-TranslateCron(AllocatorPtr alloc, SocketDescriptor s,
-	      const char *partition_name, const char *listener_tag,
-	      const char *user, const char *uri,
-	      const char *param)
-{
-	SendTranslateCron(s, partition_name, listener_tag, user, uri, param);
-	return ReceiveTranslateResponse(alloc, s);
-}
