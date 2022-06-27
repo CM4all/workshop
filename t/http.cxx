a8f50da3b2074b547ac8ddea917647b8b5340b07
Max Kellermann <mk@cm4all.com>
2022-06-27 19:35:29 +0200

    t/http: indent with tabs

--- t/http.cxx
+++ t/http.cxx
@@ -16,49 +16,49 @@
 
 class MyResponseHandler final : public CurlResponseHandler {
 public:
-    void OnHeaders(unsigned status, Curl::Headers &&headers) {
-        printf("Status: %u\n", status);
-        for (const auto &i : headers)
-            printf("%s: %s\n", i.first.c_str(), i.second.c_str());
-        printf("\n");
-    }
+	void OnHeaders(unsigned status, Curl::Headers &&headers) {
+		printf("Status: %u\n", status);
+		for (const auto &i : headers)
+			printf("%s: %s\n", i.first.c_str(), i.second.c_str());
+		printf("\n");
+	}
 
-    void OnData(ConstBuffer<void> data) {
-        fwrite(data.data, 1, data.size, stdout);
-    }
+	void OnData(ConstBuffer<void> data) {
+		fwrite(data.data, 1, data.size, stdout);
+	}
 
-    void OnEnd() {
-    }
+	void OnEnd() {
+	}
 
-    void OnError(std::exception_ptr ep) noexcept {
-        try {
-            std::rethrow_exception(ep);
-        } catch (...) {
-            PrintException(std::current_exception());
-        }
-    }
+	void OnError(std::exception_ptr ep) noexcept {
+		try {
+			std::rethrow_exception(ep);
+		} catch (...) {
+			PrintException(std::current_exception());
+		}
+	}
 };
 
 int
 main(int argc, char **argv)
 try {
-    if (argc != 2)
-        throw std::runtime_error("Usage: http URL");
+	if (argc != 2)
+		throw std::runtime_error("Usage: http URL");
 
-    const char *const url = argv[1];
+	const char *const url = argv[1];
 
-    ScopeCurlInit curl_init;
+	ScopeCurlInit curl_init;
 
-    EventLoop event_loop;
-    CurlGlobal curl_global(event_loop);;
+	EventLoop event_loop;
+	CurlGlobal curl_global(event_loop);;
 
-    MyResponseHandler handler;
-    CurlRequest request(curl_global, url, handler);
+	MyResponseHandler handler;
+	CurlRequest request(curl_global, url, handler);
 
-    event_loop.Dispatch();
+	event_loop.Dispatch();
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 } catch (...) {
-    PrintException(std::current_exception());
-    return EXIT_FAILURE;
+	PrintException(std::current_exception());
+	return EXIT_FAILURE;
 }
