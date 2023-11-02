504ba26289a61cb5fc3206d24df8f0e1389c5f83
Max Kellermann <mk@cm4all.com>
2023-11-02 13:53:22 +0100

    Hook: include cleanup

--- src/Hook.hxx
+++ src/Hook.hxx
@@ -4,28 +4,9 @@
 
 #pragma once
 
-#include "control/Handler.hxx"
-#include "event/Loop.hxx"
-#include "event/ShutdownListener.hxx"
-#include "event/SignalEvent.hxx"
-#include "event/DeferEvent.hxx"
-#include "event/systemd/Watchdog.hxx"
-#include "spawn/Registry.hxx"
 #include "spawn/Hook.hxx"
-#include "lib/curl/Init.hxx"
-#include "lib/curl/Global.hxx"
-#include "io/Logger.hxx"
 
-#include <forward_list>
-
-struct Config;
-class UniqueSocketDescriptor;
-class SpawnServerClient;
-class CurlGlobal;
 class MultiLibrary;
-class ControlServer;
-class WorkshopPartition;
-class CronPartition;
 
 class WorkshopSpawnHook final : public SpawnHook {
 	MultiLibrary *const library;
