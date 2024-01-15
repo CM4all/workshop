7897cc70d5c8f7408b6550a22f33cc20e94a502f
Max Kellermann <mk@cm4all.com>
2024-01-15 17:18:00 +0100

    cron/SpawnOperator: fix indent

--- src/cron/SpawnOperator.cxx
+++ src/cron/SpawnOperator.cxx
@@ -33,39 +33,39 @@ CronSpawnOperator::~CronSpawnOperator() noexcept = default;
 void
 CronSpawnOperator::Spawn(PreparedChildProcess &&p,
 			 SocketDescriptor pond_socket)
-	try {
-		if (!p.stderr_fd.IsDefined()) {
-			/* no STDERR destination configured: the default is to capture
-			   it and save in the cronresults table */
-			auto [r, w] = CreatePipe();
+try {
+	if (!p.stderr_fd.IsDefined()) {
+		/* no STDERR destination configured: the default is to capture
+		   it and save in the cronresults table */
+		auto [r, w] = CreatePipe();
 
-			p.SetStderr(std::move(w));
-			if (!p.stdout_fd.IsDefined())
-				/* capture STDOUT as well */
-				p.stdout_fd = p.stderr_fd;
+		p.SetStderr(std::move(w));
+		if (!p.stdout_fd.IsDefined())
+			/* capture STDOUT as well */
+			p.stdout_fd = p.stderr_fd;
 
-			output_capture = std::make_unique<PipePondAdapter>(GetEventLoop(),
-									   std::move(r),
-									   8192,
-									   pond_socket,
-									   job.account_id);
-		}
-
-		/* change to home directory (if one was set) */
-		p.chdir = p.ns.GetJailedHome();
-
-		pid = spawn_service.SpawnChildProcess(job.id.c_str(),
-						      std::move(p));
-		pid->SetExitListener(*this);
-
-		logger(2, "running");
-
-		/* kill after 5 minutes */
-		timeout_event.Schedule(std::chrono::minutes(5));
-	} catch (...) {
-		Finish(-1, GetFullMessage(std::current_exception()).c_str());
-		throw;
+		output_capture = std::make_unique<PipePondAdapter>(GetEventLoop(),
+								   std::move(r),
+								   8192,
+								   pond_socket,
+								   job.account_id);
 	}
+
+	/* change to home directory (if one was set) */
+	p.chdir = p.ns.GetJailedHome();
+
+	pid = spawn_service.SpawnChildProcess(job.id.c_str(),
+					      std::move(p));
+	pid->SetExitListener(*this);
+
+	logger(2, "running");
+
+	/* kill after 5 minutes */
+	timeout_event.Schedule(std::chrono::minutes(5));
+} catch (...) {
+	Finish(-1, GetFullMessage(std::current_exception()).c_str());
+	throw;
+}
 
 void
 CronSpawnOperator::Cancel() noexcept
