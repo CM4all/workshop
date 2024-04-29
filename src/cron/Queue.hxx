CM4all/workshop

commit 109ee63a928f980c33947db90e3acaa1b0349462

cron: connect to database only if partition is enabled

src/cron/Queue.hxx
@@ -68,10 +68,6 @@ public:
 		return node_name.c_str();
 	}
 
-	void Connect() noexcept {
-		db.Connect();
-	}
-
 	[[gnu::pure]]
 	std::string GetNow() {
 		ScheduleCheckNotify();

src/Instance.cxx
@@ -57,8 +57,6 @@ Instance::Start()
 
 	for (auto &i : partitions)
 		i.Start();
-	for (auto &i : cron_partitions)
-		i.Start();
 
 	if (library)
 		UpdateLibraryAndFilter(true);

src/cron/Partition.hxx
@@ -61,10 +61,6 @@ public:
 		return workplace.IsEmpty();
 	}
 
-	void Start() noexcept {
-		queue.Connect();
-	}
-
 	void BeginShutdown() noexcept;
 
 	void SetStateEnabled(bool _enabled) noexcept {

src/cron/Queue.cxx
@@ -33,8 +33,17 @@ CronQueue::~CronQueue() noexcept = default;
 void
 CronQueue::CheckEnabled() noexcept
 {
-	if (IsEnabled() && db.IsReady())
-		ScheduleClaim();
+	if (IsEnabled()) {
+		if (!db.IsDefined())
+			db.Connect();
+		else if (db.IsReady())
+			ScheduleClaim();
+	} else {
+		if (db.IsDefined() && !db.IsReady())
+			db.Disconnect();
+
+		// TODO disconnect if idle?
+	}
 }
 
 void

commit db6d4b7e4001a684ad9143e1228dde7c87886402

cron/Queue: fix initial connect
By initializing `enabled_state` to false, the initial call to
SetStateEnabled() will trigger the connection.  Fixes a regression in
commit 109ee63a928f980c33947db90e3acaa1b0349462

src/cron/Queue.hxx
@@ -38,7 +38,7 @@ class CronQueue final : private Pg::AsyncConnectionHandler {
 	/**
 	 * Was the queue enabled by #StateDirectories?
 	 */
-	bool enabled_state = true;
+	bool enabled_state = false;
 
 	/**
 	 * Was the queue enabled by the administrator?  Also used