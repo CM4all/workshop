CM4all/workshop

commit 96b3c5f7fc99866ddb1682cb242179b0cc7d6431

cron/CurlOperator: perform HTTP requests in containerized user process
By using the same network namespace, users gets access only to
networks that have been made available to their namespace.  Same
restrictions as invoking "curl" in a SSH session.

src/cron/CurlOperator.cxx
@@ -5,68 +5,165 @@
 #include "CurlOperator.hxx"
 #include "Result.hxx"
 #include "CaptureBuffer.hxx"
+#include "lib/curl/Adapter.hxx"
+#include "lib/curl/Easy.hxx"
+#include "lib/curl/Handler.hxx"
+#include "lib/curl/Setup.hxx"
+#include "spawn/ChildOptions.hxx"
+#include "spawn/Interface.hxx"
+#include "spawn/Prepared.hxx"
+#include "spawn/ProcessHandle.hxx"
+#include "net/SocketError.hxx"
+#include "net/SocketPair.hxx"
+#include "net/SocketProtocolError.hxx"
+#include "net/UniqueSocketDescriptor.hxx"
 #include "util/SpanCast.hxx"
 #include "util/StringCompare.hxx"
 
-#include <algorithm>
-
 using std::string_view_literals::operator""sv;
 
-CronCurlOperator::CronCurlOperator(CurlGlobal &_global,
-				   const char *url) noexcept
-	:request(_global, url, *this)
-{
-}
+namespace {
 
-CronCurlOperator::~CronCurlOperator() noexcept = default;
+class MyResponseHandler final : public CurlResponseHandler {
+public:
+	bool is_text = false;
+	SocketDescriptor socket;
+	std::exception_ptr error;
 
-void
-CronCurlOperator::Start()
-{
-	request.Start();
-}
+	void OnHeaders(HttpStatus status, Curl::Headers &&headers) override {
+		(void)socket.Write(ReferenceAsBytes(status));
 
-void
-CronCurlOperator::OnHeaders(HttpStatus _status, Curl::Headers &&headers)
-{
-	status = _status;
-
-	const auto ct = headers.find("content-type"sv);
-	if (ct != headers.end()) {
-		const char *content_type = ct->second.c_str();
-		if (StringStartsWith(content_type, "text/"sv))
-			/* capture the response body if it's text */
-			output_capture = std::make_unique<CaptureBuffer>(8192);
+		if (const auto i = headers.find("content-type"sv);
+		    i != headers.end())
+			is_text = i->second.starts_with("text/"sv);
 	}
-}
 
-void
-CronCurlOperator::OnData(std::span<const std::byte> _src)
-{
-	if (output_capture) {
-		const auto src = ToStringView(_src);
-		auto w = output_capture->Write();
-		size_t nbytes = std::min(w.size(), src.size());
-		std::copy_n(src.begin(), nbytes, w.begin());
-		output_capture->Append(nbytes);
+	void OnData(std::span<const std::byte> data) override {
+		if (!is_text)
+			return;
+
+		if (socket.Write(data) < 0)
+			throw MakeSocketError("send() failed");
 	}
+
+	void OnEnd() override {}
+
+	void OnError(std::exception_ptr _error) noexcept override {
+		error = std::move(_error);
+	}
+};
+
+static CurlEasy
+ReadRequest(SocketDescriptor s)
+{
+	char url[4096];
+
+	if (const auto nbytes = s.Receive(std::as_writable_bytes(std::span{url}));
+	    nbytes < 0)
+		throw MakeSocketError("recvmsg() failed");
+	else if (static_cast<std::size_t>(nbytes) == sizeof(url))
+		throw SocketBufferFullError{};
+	else
+		url[nbytes] = 0;
+
+	return CurlEasy{url};
 }
 
-void
-CronCurlOperator::OnEnd()
+static int
+SpawnCurlFunction(PreparedChildProcess &&)
 {
-	CronResult result{
-		.exit_status = static_cast<int>(status),
+	SocketDescriptor control{3};
+
+	auto easy = ReadRequest(control);
+	Curl::Setup(easy);
+
+	MyResponseHandler handler;
+	CurlResponseHandlerAdapter adapter{handler};
+	adapter.Install(easy);
+
+	easy.Perform();
+	adapter.Done(CURLE_OK);
+
+	if (handler.error)
+		std::rethrow_exception(handler.error);
+
+	return 0;
+}
+
+static std::pair<UniqueSocketDescriptor, std::unique_ptr<ChildProcessHandle>>
+SpawnCurl(SpawnService &spawn_service, const char *name,
+	  const ChildOptions &options)
+{
+	// TODO this is a horrible and inefficient kludge
+	auto [control_socket, control_socket_for_child] = CreateSocketPair(SOCK_SEQPACKET);
+
+	PreparedChildProcess p;
+	p.exec_function = SpawnCurlFunction;
+	p.args.push_back("dummy");
+	p.ns = {ShallowCopy{}, options.ns};
+	p.ns.enable_pid = false;
+	p.ns.enable_cgroup = false;
+	p.ns.enable_ipc = false;
+	p.ns.pid_namespace = nullptr;
+	p.uid_gid = options.uid_gid;
+#ifdef HAVE_LIBSECCOMP
+	p.forbid_multicast = options.forbid_multicast;
+	p.forbid_bind = options.forbid_bind;
+#endif // HAVE_LIBSECCOMP
+	p.SetControl(std::move(control_socket_for_child));
+
+	return {
+		std::move(control_socket),
+		spawn_service.SpawnChildProcess(name, std::move(p)),
 	};
+}
 
-	if (output_capture)
-		result.log = std::move(*output_capture).NormalizeASCII();
+} // anonymous namespace
 
-	Finish(std::move(result));
+CronCurlOperator::CronCurlOperator(EventLoop &event_loop) noexcept
+	:socket(event_loop, BIND_THIS_METHOD(OnSocketReady))
+{
+}
+
+CronCurlOperator::~CronCurlOperator() noexcept
+{
+	socket.Close();
 }
 
 void
-CronCurlOperator::OnError(std::exception_ptr ep) noexcept
+CronCurlOperator::Start(SpawnService &spawn_service, const char *name,
+			const ChildOptions &options, const char *url)
 {
-	Finish(CronResult::Error(ep));
+	auto [_socket, _pid] = SpawnCurl(spawn_service, name, options);
+
+	if (_socket.Write(AsBytes(std::string_view{url})) < 0)
+		throw MakeSocketError("Failed to send");
+
+	pid = std::move(_pid);
+
+	socket.Open(_socket.Release());
+	socket.ScheduleRead();
+}
+
+void
+CronCurlOperator::OnSocketReady(unsigned) noexcept
+{
+	if (status == HttpStatus{}) {
+		if (socket.GetSocket().ReadNoWait(ReferenceAsWritableBytes(status)) != static_cast<ssize_t>(sizeof(status)) ||
+		    status == HttpStatus{})
+			Finish(CronResult::Error("Failed to read status"));
+
+		return;
+	}
+
+	auto w = capture.Write();
+	const auto nbytes = socket.GetSocket().ReadNoWait(std::as_writable_bytes(w));
+	if (nbytes > 0) {
+		capture.Append(nbytes);
+	} else {
+		Finish(CronResult{
+				.log = std::move(capture).NormalizeASCII(),
+				.exit_status = static_cast<int>(status),
+			});
+	}
 }

src/Instance.cxx
@@ -27,7 +27,6 @@ Instance::Instance(const Config &config,
 					     /* disable "verify", we
 						do it via SpawnHook */
 					     false)),
-	 curl(event_loop),
 	 library(std::move(_library))
 {
 	shutdown_listener.Enable();
@@ -39,7 +38,7 @@ Instance::Instance(const Config &config,
 					 BIND_THIS_METHOD(OnPartitionIdle));
 
 	for (const auto &i : config.cron_partitions)
-		cron_partitions.emplace_front(event_loop, *spawn_service, curl,
+		cron_partitions.emplace_front(event_loop, *spawn_service,
 					      config, i,
 					      BIND_THIS_METHOD(OnPartitionIdle));
 

src/Instance.hxx
@@ -11,7 +11,6 @@
 #include "event/DeferEvent.hxx"
 #include "spawn/Registry.hxx"
 #include "lib/curl/Init.hxx"
-#include "lib/curl/Global.hxx"
 #include "io/Logger.hxx"
 #include "io/StateDirectories.hxx"
 #include "config.h"
@@ -25,7 +24,6 @@
 struct Config;
 class UniqueSocketDescriptor;
 class SpawnServerClient;
-class CurlGlobal;
 class MultiLibrary;
 namespace BengControl { class Server; }
 class WorkshopPartition;
@@ -49,7 +47,6 @@ class Instance final : BengControl::Handler {
 	std::unique_ptr<SpawnServerClient> spawn_service;
 
 	ScopeCurlInit curl_init;
-	CurlGlobal curl;
 
 	const StateDirectories state_directories;
 

src/cron/CurlOperator.hxx
@@ -5,36 +5,38 @@
 #pragma once
 
 #include "Operator.hxx"
-#include "lib/curl/Handler.hxx"
-#include "lib/curl/Request.hxx"
+#include "event/SocketEvent.hxx"
+#include "CaptureBuffer.hxx"
 
+#include <cstdint>
 #include <memory>
 
-class CaptureBuffer;
+enum class HttpStatus : uint_least16_t;
+struct ChildOptions;
+class SpawnService;
+class ChildProcessHandle;
 
 /**
  * A #CronJob which sends a HTTP GET request to a specific URL.
  */
 class CronCurlOperator final
-	: public CronOperator, CurlResponseHandler
+	: public CronOperator
 {
-	CurlRequest request;
+	SocketEvent socket;
+
+	std::unique_ptr<ChildProcessHandle> pid;
+
+	CaptureBuffer capture{8192};
 
 	HttpStatus status{};
 
-	std::unique_ptr<CaptureBuffer> output_capture;
-
 public:
-	CronCurlOperator(CurlGlobal &_global,
-			 const char *url) noexcept;
+	CronCurlOperator(EventLoop &event_loop) noexcept;
 	~CronCurlOperator() noexcept override;
 
-	void Start();
+	void Start(SpawnService &spawn_service, const char *name,
+		   const ChildOptions &options, const char *url);
 
 private:
-	/* virtual methods from CurlResponseHandler */
-	void OnHeaders(HttpStatus status, Curl::Headers &&headers) override;
-	void OnData(std::span<const std::byte> src) override;
-	void OnEnd() override;
-	void OnError(std::exception_ptr ep) noexcept override;
+	void OnSocketReady(unsigned events) noexcept;
 };

src/cron/Partition.cxx
@@ -10,7 +10,6 @@
 
 CronPartition::CronPartition(EventLoop &event_loop,
 			     SpawnService &_spawn_service,
-			     CurlGlobal &_curl,
 			     const Config &root_config,
 			     const CronPartitionConfig &config,
 			     BoundMethod<void() noexcept> _idle_callback)
@@ -28,7 +27,7 @@ CronPartition::CronPartition(EventLoop &event_loop,
 		       config.database.c_str(), config.database_schema.c_str(),
 		       [this](CronJob &&job){ OnJob(std::move(job)); }),
 	 workplace(_spawn_service, email_service.get(), pond_socket,
-		   _curl, *this,
+		   *this,
 		   root_config.concurrency),
 	 idle_callback(_idle_callback),
 	 default_timeout(config.default_timeout)

src/cron/Partition.hxx
@@ -45,7 +45,6 @@ class CronPartition final : ExitListener {
 public:
 	CronPartition(EventLoop &event_loop,
 		      SpawnService &_spawn_service,
-		      CurlGlobal &_curl,
 		      const Config &root_config,
 		      const CronPartitionConfig &config,
 		      BoundMethod<void() noexcept> _idle_callback);

src/cron/Workplace.cxx
@@ -126,13 +126,11 @@ CronWorkplace::Running::SetResult(const CronResult &result) noexcept
 CronWorkplace::CronWorkplace(SpawnService &_spawn_service,
 			     EmailService *_email_service,
 			     SocketDescriptor _pond_socket,
-			     CurlGlobal &_curl,
 			     ExitListener &_exit_listener,
 			     std::size_t _max_operators)
 	:spawn_service(_spawn_service),
 	 email_service(_email_service),
 	 pond_socket(_pond_socket),
-	 curl(_curl),
 	 exit_listener(_exit_listener),
 	 max_operators(_max_operators)
 {
@@ -212,11 +210,12 @@ MakeSpawnOperator(EventLoop &event_loop, SpawnService &spawn_service,
 }
 
 static std::unique_ptr<CronOperator>
-MakeCurlOperator(CurlGlobal &curl_global,
-		 const char *url)
+MakeCurlOperator(EventLoop &event_loop, SpawnService &spawn_service,
+		 const CronJob &job, const char *url,
+		 const TranslateResponse &response)
 {
-	auto o = std::make_unique<CronCurlOperator>(curl_global, url);
-	o->Start();
+	auto o = std::make_unique<CronCurlOperator>(event_loop);
+	o->Start(spawn_service, job.id.c_str(), response.child_options, url);
 	return std::unique_ptr<CronOperator>(std::move(o));
 }
 
@@ -256,8 +255,11 @@ CronWorkplace::Running::MakeOperator(SocketAddress translation_socket,
 		timeout_event.Schedule(response.timeout);
 
 	if (IsURL(job.command))
-		co_return MakeCurlOperator(workplace.curl,
-					   job.command.c_str());
+		co_return MakeCurlOperator(GetEventLoop(),
+					   workplace.GetSpawnService(),
+					   job,
+					   job.command.c_str(),
+					   response);
 	else
 		co_return MakeSpawnOperator(GetEventLoop(),
 					    workplace.GetSpawnService(),

src/cron/Workplace.hxx
@@ -10,7 +10,6 @@
 struct CronJob;
 class SpawnService;
 class EmailService;
-class CurlGlobal;
 class CronQueue;
 class ExitListener;
 
@@ -19,7 +18,6 @@ class CronWorkplace {
 	EmailService *const email_service;
 	const SocketDescriptor pond_socket;
 
-	CurlGlobal &curl;
 	ExitListener &exit_listener;
 
 	class Running;
@@ -33,7 +31,6 @@ public:
 	CronWorkplace(SpawnService &_spawn_service,
 		      EmailService *_email_service,
 		      SocketDescriptor _pond_socket,
-		      CurlGlobal &_curl,
 		      ExitListener &_exit_listener,
 		      std::size_t _max_operators);
 

commit dcc7340b3872d05a96a6647832211fde6966f43c

cron/CurlOperator: initialize MyResponseHandler::socket
This was broken by commit 96b3c5f7fc9 but we never noticed it, d'oh!

src/cron/CurlOperator.cxx
@@ -33,6 +33,9 @@ public:
 	SocketDescriptor socket;
 	std::exception_ptr error;
 
+	explicit MyResponseHandler(SocketDescriptor _socket) noexcept
+		:socket(_socket) {}
+
 	void OnHeaders(HttpStatus status, Curl::Headers &&headers) override {
 		(void)socket.Write(ReferenceAsBytes(status));
 
@@ -80,7 +83,7 @@ SpawnCurlFunction(PreparedChildProcess &&)
 	auto easy = ReadRequest(control);
 	Curl::Setup(easy);
 
-	MyResponseHandler handler;
+	MyResponseHandler handler{control};
 	CurlResponseHandlerAdapter adapter{handler};
 	adapter.Install(easy);
 