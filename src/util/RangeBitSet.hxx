b5088219eac091a472abcd95169815efe7354d51
Max Kellermann <max.kellermann@ionos.com>
2025-09-29 11:29:14 +0200

    util/RangeBitSet: use std::size_t

--- src/util/RangeBitSet.hxx
+++ src/util/RangeBitSet.hxx
@@ -10,7 +10,7 @@
  * A variant of std::bitset which has a lower and upper bound.  The
  * "MIN" offset is applied to all internal bits.
  */
-template<size_t MIN, size_t MAX>
+template<std::size_t MIN, std::size_t MAX>
 class RangeBitSet {
 	using BitSet = std::bitset<MAX - MIN + 1>;
 
@@ -19,31 +19,31 @@ class RangeBitSet {
 public:
 	constexpr bool operator==(const RangeBitSet<MIN, MAX> &other) const noexcept = default;
 
-	constexpr size_t size() const noexcept {
+	constexpr std::size_t size() const noexcept {
 		return b.size();
 	}
 
-	constexpr size_t count() const noexcept {
+	constexpr std::size_t count() const noexcept {
 		return b.count();
 	}
 
-	constexpr size_t all() const noexcept {
+	constexpr std::size_t all() const noexcept {
 		return b.all();
 	}
 
-	constexpr size_t any() const noexcept {
+	constexpr std::size_t any() const noexcept {
 		return b.any();
 	}
 
-	constexpr size_t none() const noexcept {
+	constexpr std::size_t none() const noexcept {
 		return b.none();
 	}
 
-	constexpr bool operator[](size_t pos) const noexcept {
+	constexpr bool operator[](std::size_t pos) const noexcept {
 		return b[pos - MIN];
 	}
 
-	constexpr typename BitSet::reference operator[](size_t pos) noexcept {
+	constexpr typename BitSet::reference operator[](std::size_t pos) noexcept {
 		return b[pos - MIN];
 	}
 };
